3598b35b7173db06c976d6cf528b08fff7470016
Charles Musabi <musabicharles91@gmail.com>
2024-08-27 16:09:14 +0300

    Coverter file

---
+++ convert.c
@@ -0,0 +1,24 @@
+#include "printf.h"
+/**
+ * convert - converts output to binary, base 10, octal or hexadecimal
+ * @num: number to be converted
+ * @base: base to be adjusted
+ * Return: pointer to the address of the converted number.
+ */
+char *convert(unsigned int num, int base)
+{
+	static char Representation[] = "0123456789ABCDEF";
+	static char buffer[50];
+	char *ptr = &buffer[49];
+
+	*ptr = '\0';
+
+	while (num != 0)
+	{
+		*--ptr = Representation[num % base];
+		num /= base;
+	}
+
+	return(ptr);
+}
+
