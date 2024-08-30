9458845821c5858c8bfae411b7acf46bc3b7b684
Charles Musabi <musabicharles91@gmail.com>
2024-08-30 10:37:02 +0300

    Function file

---
+++ function.c
@@ -0,0 +1,35 @@
+#include "printf.h"
+/**
+ *
+ */
+int print_char(va_list args)
+{
+}
+
+/**
+ *
+ */
+int print_int(va_list args)
+{
+}
+
+/**
+ *
+ */
+int print_octal(va_list args)
+{
+}
+
+/**
+ *
+ */
+int print_string(va_list args)
+{
+}
+
+/**
+ */
+int print_hex(va_list args)
+{
+}
+
