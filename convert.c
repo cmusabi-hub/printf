083e5e08c7633ddde2a21052688633dbd4fc93f2
Charles Musabi <musabicharles91@gmail.com>
2024-08-30 10:08:27 +0300

    Convert function file

--- convert.c
+++ convert.c
@@ -5,7 +5,7 @@
  * @base: base to be adjusted
  * Return: pointer to the address of the converted number.
  */
-char *converT(unsigned int num, int base)
+char *convert(unsigned int num, int base, char formater)
 {
 	static char Representation[] = "0123456789ABCDEF";
 	static char representation[] = "0123456789abcdef";
