9099c78f9976fb6b3e87ad5bfbc3ccfe89ed3504
Charles Musabi <musabicharles91@gmail.com>
2024-09-01 23:50:26 +0300

    Printf Header file

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+#include "printf.h"
 
 /**
  * main - Entry point
@@ -13,7 +13,7 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
-
+/**
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
@@ -28,8 +28,10 @@ int main(void)
     printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+**/   
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
+/**   
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
     _printf("Address:[%p]\n", addr);
@@ -40,6 +42,7 @@ int main(void)
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+**/
     return (0);
 }
 
--- printf.h
+++ printf.h
@@ -1,6 +1,8 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
 
+#define UNUSED(x) void(x)
+
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
