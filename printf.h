c6d5c2a29aea14c82c96ed61bd8d0d502067cf6f
Charles Musabi <musabicharles91@gmail.com>
2024-09-04 18:48:59 +0300

    Printf Header file

--- printf.h
+++ printf.h
@@ -4,6 +4,7 @@
 #define UNUSED(x) void(x)
 
 #include <stdarg.h>
+#include <stddef.f>
 
 int _printf(const char *format, ...);
 int convert(unsigned int, int);
@@ -21,7 +22,7 @@ typedef struct format_specifier {
 formatters  formatters_functions[] = {
 	{'c', print_char},
 	{'d', print_int},
-	{'i', print_int}.
+	{'i', print_int},
 	{'o', print_octal},
 	{'s', print_string},
 	{'x', print_hex},
